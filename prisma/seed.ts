e8689114ee57dd83c8ab42b0acc4f8eb84121061
nekorytaylor666 <nekorytaylor2@gmail.com>
2023-09-21 11:08:03 +0800

    chore: copied prisma schema from old project

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,43 +1,58 @@
-import { PrismaClient, type users } from '@prisma/client';
-import { faker } from '@faker-js/faker';
-import logger from '../src/lib/logger';
-import { HR } from '../src/utils/helper';
-
+const { PrismaClient, UserRole } = require('@prisma/client');
 const prisma = new PrismaClient();
-const seedUsers = async (): Promise<void> => {
-  const fakeUsers = faker.helpers.uniqueArray<users>(
-    () => ({
-      id: faker.datatype.uuid(),
-      name: faker.name.fullName(),
-      email: faker.internet.email(),
-      phone: faker.phone.number(),
-    }),
-    3
-  );
-  const users = await prisma.users.createMany({ data: fakeUsers });
-  logger.info(`
-    \r${HR('white', '-', 30)}
-    \rSeed completed for model: user
-    \rcount: ${users.count}
-    \r${HR('white', '-', 30)}
-  `);
-};
 
-async function seed(): Promise<void> {
-  await Promise.all([seedUsers()]);
+async function main() {
+  const adminUser = await prisma.user.create({
+    data: {
+      fullName: 'admin',
+      email: 'admin@example.com',
+      phoneNumber: '1234567890',
+      updatedAt: new Date(),
+      createdAt: new Date(),
+      role: UserRole.ADMIN,
+    },
+  });
+
+  const user1 = await prisma.user.create({
+    data: {
+      fullName: 'alice',
+      email: 'alice@example.com',
+      phoneNumber: '1234567890',
+      updatedAt: new Date(),
+      createdAt: new Date(),
+    },
+  });
+
+  const course = await prisma.course.create({
+    data: {
+      authorId: adminUser.id,
+      lessons: {
+        create: [
+          {
+            authorId: adminUser.id,
+            title: 'Структура программы',
+            mdxContentPath: 'chatgpt-course/lessons/1/content.mdx',
+            thumbnailPath: 'chatgpt-course/lessons/1/lesson_thumbnail.png',
+            order: 1,
+          },
+        ],
+      },
+      title: 'Как работает ChatGPT на самом деле?',
+      slug: 'chatgpt-course',
+      thumbnailPath: 'chatgpt-course/course_thumbnail.png',
+      description:
+        'Этот курс расскажет вам о том, как работает ChatGPT на самом деле.',
+      updatedAt: new Date(),
+      createdAt: new Date(),
+    },
+  });
 }
 
-async function main(): Promise<void> {
-  let isError: boolean = false;
-  try {
-    await seed();
-  } catch (e) {
-    isError = true;
-    logger.error(e);
-  } finally {
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
     await prisma.$disconnect();
-    process.exit(isError ? 1 : 0);
-  }
-}
-
-void main();
+  });
